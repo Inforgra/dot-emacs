7e42f8946557420cd1e2de981580bad93fd3e4e3
inforgra <inforgrax@gmail.com>
2024-08-10 21:26:58 +0900

    markdown-mode 내 visual-fill-column, display-line-numbers 모드 추가

--- lisp/dot-emacs-dev-markdown.el
+++ lisp/dot-emacs-dev-markdown.el
@@ -7,9 +7,13 @@
 (require 'flycheck)
 (require 'whitespace-cleanup-mode)
 (require 'paren)
+(require 'visual-fill-column)
 
 (use-package markdown-mode
-  :ensure t)
+  :ensure t
+  :hook
+  (markdown-mode . display-line-numbers-mode)
+  (markdown-mode . visual-fill-column-mode))
 
 (provide 'dot-emacs-dev-markdown)
 ;;; dot-emacs-dev-markdown.el ends here
