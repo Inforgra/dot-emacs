81bbd04482c39ba3f018855940310bb3b9cebfe7
inforgra <inforgrax@gmail.com>
2024-08-30 16:26:03 +0900

    add hook visual-line-fill-column to org-mode

--- lisp/dot-emacs-org.el
+++ lisp/dot-emacs-org.el
@@ -26,7 +26,7 @@
   (require 'ox-markdown nil t)
   :hook
   (org-mode . real-auto-save-mode)
-  (org-mode . visual-line-mode)
+  (org-mode . visual-line-fill-column-mode)
   :custom
   (org-export-allow-bind-keywords t)
   (org-agenda-files '("~/.org/inbox.org" "~/.org/gtd.org"))
