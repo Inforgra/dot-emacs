6fa3302032b96f823c8ae66c66783de91568d8d2
inforgra <inforgrax@gmail.com>
2025-01-29 19:57:11 +0900

    haskell 개발환경롤백

--- lisp/dot-emacs-dev-haskell.el
+++ lisp/dot-emacs-dev-haskell.el
@@ -8,21 +8,21 @@
 (use-package helm-lsp
   :ensure t)
 
-(use-package haskell-mode
-  :ensure t
-  :bind
-  (:map interactive-haskell-mode-map
-        ("M-/" . helm-lsp-code-actions)
-        ("M-?" . xref-find-references)
-        ("M-." . xref-find-definitions))
-  :hook
-  (haskell-mode . company-mode)
-  (haskell-mode . display-line-numbers-mode)
-  (haskell-mode . flycheck-mode)
-  (haskell-mode . lsp-mode)
-  (haskell-mode . lsp-completion-mode)
-  (haskell-mode . show-paren-mode)
-  (haskell-mode . whitespace-cleanup-mode))
+;; (use-package haskell-mode
+;;   :ensure t
+;;   :bind
+;;   ("M-/" . helm-lsp-code-actions)
+;;   (:map interactive-haskell-mode-map
+;;         ("M-?" . xref-find-references)
+;;         ("M-." . xref-find-definitions))
+;;   :hook
+;;   (haskell-mode . company-mode)
+;;   (haskell-mode . display-line-numbers-mode)
+;;   (haskell-mode . flycheck-mode)
+;;   (haskell-mode . lsp-mode)
+;;   (haskell-mode . lsp-completion-mode)
+;;   (haskell-mode . show-paren-mode)
+;;   (haskell-mode . whitespace-cleanup-mode))
 
 (provide 'dot-emacs-dev-haskell)
 ;;; dot-emacs-dev-haskell.el ends here.
